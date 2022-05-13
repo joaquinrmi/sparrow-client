ef49d07f2d36029b09b250a731b21484d51cdd98
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-13 20:28:09 -0300

    Update App component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import NavigationController from "../components/navigation_controller/";
+
+import Welcome from "../pages/welcome";
 
 import "./app.scss";
 
 const App: React.FunctionComponent = () =>
 {
-    return <></>;
+    return <NavigationController>
+        <Welcome />
+    </NavigationController>;
 };
 
 export default App;
