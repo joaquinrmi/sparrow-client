925193a9de51f9759add97ac8a1bd93508496333
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-13 23:57:11 -0300

    Add 'userSession' state to App component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Welcome from "../pages/welcome";
+import SessionData from "../session_data";
 
 import "./app.scss";
 
 const App: React.FunctionComponent = () =>
 {
+    const [ userSession, setUserSession ] = useState<SessionData>({
+        logged: false,
+        user: {
+            handle: "",
+            name: "",
+            picture: ""
+        }
+    });
+
     return <Welcome />;
 };
 
