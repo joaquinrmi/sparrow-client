51645c94e3c1738ada6e0b9a4f2689c09007494b
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-15 03:44:06 -0300

    Send 'changePage()' to FirstPage component

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,21 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import FirstPage from "./components/first_page";
+import SignupForm from "./signup_form";
+import SignupFormSet from "./signup_form_set";
 
 import "./signup.scss";
-import SignupForm from "./signup_form";
 
 const Signup: React.FunctionComponent = () =>
 {
-    const [ signupData, setSignupData ] = useState<SignupForm>({
-        name: "",
-        email: "",
-        birthdate: new Date(),
-        handle: "",
-        password: "",
-        repassword: ""
+    const [ state, setSate ] = useState<{
+        currentPage: number,
+        signupData: SignupForm
+    }>({
+        currentPage: 0,
+        signupData: {
+            name: "",
+            email: "",
+            birthdate: new Date(),
+            handle: "",
+            password: "",
+            repassword: ""
+        }
     });
 
-    return <FirstPage signupData={signupData} />;
+    const changePage = (page: number, data: SignupFormSet) =>
+    {
+        if(page !== 0 && page !== 1)
+        {
+            page = 0;
+        }
+
+        setSate((currentState) =>
+        {
+            return {
+                currentPage: page,
+                signupData: {
+                    ...currentState.signupData,
+                    ...data
+                }
+            };
+        });
+    };
+
+    return <FirstPage signupData={state.signupData} changePage={changePage} />;
 };
 
 export default Signup;
