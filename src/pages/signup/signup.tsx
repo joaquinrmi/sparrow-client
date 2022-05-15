98c8854d66c46809232daebcfb9dd6c08cfacfa4
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-15 03:20:01 -0300

    Add SignupFormSet type

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,89 +1,25 @@
 import React, { useState, useEffect } from "react";
-
-import FormInput, { FormInputElement } from "../../components/form_input/";
-import DateInput from "../../components/date_input";
-import Button, { ButtonStyle } from "../../components/button";
+import FirstPage from "./components/first_page";
 
 import "./signup.scss";
+import SignupForm from "./signup_form";
 
 const Signup: React.FunctionComponent = () =>
 {
-    const [ enableNext, setEnableNext ] = useState(false);
+    const [ signupData, setSignupData ] = useState<SignupForm>({
+        name: "",
+        email: "",
+        birthdate: new Date(),
+        handle: "",
+        password: "",
+        repassword: ""
+    });
 
-    useEffect(() =>
-    {
-        const nameInput = document.getElementById("signup-name") as FormInputElement;
-        const emailInput = document.getElementById("signup-email") as FormInputElement;
+    return <FirstPage />;
+};
 
-        const emailRegex = /^[a-zA-Z\-_0-9]*@[a-zA-Z\-_0-9]*\.com$/;
-
-        const checkEnableNext = () =>
-        {
-            if(nameInput.getValue().trim().length === 0)
-            {
-                setEnableNext(false);
-                return;
-            }
-
-            const email = emailInput.getValue();
-            const match = email.match(emailRegex);
-
-            if(match === null || match.length !== 1)
-            {
-                setEnableNext(false);
-                return;
-            }
-
-            setEnableNext(true);
-        };
-
-        nameInput.addEventListener("change", () =>
-        {
-            checkEnableNext();
-        });
-
-        emailInput.addEventListener("change", () =>
-        {
-            checkEnableNext();
-        });
-    },
-    []);
-
-    return <div className="signup-page">
-        <div className="form-modal">
-            <header className="signup-form-top">
-                <div id="signup-close-button">
-                    <i className="fa-solid fa-xmark"></i>
-                </div>
-            </header>
-
-            <section className="signup-form-body">
-                <h1>Crea tu cuenta</h1>
-
-                <div className="form-elements">
-                    <FormInput id="signup-name" title="Nombre" />
-
-                    <FormInput id="signup-email" title="Correo electrónico" />
-
-                    <div className="birthdate-text">
-                        <span className="birthdate-title">Fecha de nacimiento</span>
-
-                        <span>Esta información no se mostrará públicamente y, de hecho, no sirve para nada más que alargar el formulario.</span>
-                    </div>
-                    
-                    <DateInput id="signup-date-input" className="signup-date-form" />
-                </div>
-            </section>
-
-            <footer className="signup-form-bottom">
-                <div className="button-container">
-                    <Button stylePreset={ButtonStyle.Blue} disabled={!enableNext}>
-                        Siguiente
-                    </Button>
-                </div>
-            </footer>
-        </div>
-    </div>;
+type SignupFormSet = {
+    [Prop in keyof SignupForm]?: SignupForm[Prop]
 };
 
 export default Signup;
