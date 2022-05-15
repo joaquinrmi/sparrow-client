75d8976f2ca7033d141ac64e42c6411da5370c0f
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-15 03:12:38 -0300

    Add SignupForm interface

---
+++ src/pages/signup/signup_form.ts
@@ -0,0 +1,11 @@
+interface SignupForm
+{
+    name: string;
+    email: string;
+    birthdate: Date;
+    handle: string;
+    password: string;
+    repassword: string;
+}
+
+export default SignupForm;
