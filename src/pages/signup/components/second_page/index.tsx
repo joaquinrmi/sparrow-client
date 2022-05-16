fa1e5219fcf22379dd712c796ba842bddd01f7ae
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-15 23:51:21 -0300

    Add SecondPage component

---
+++ src/pages/signup/components/second_page/index.tsx
@@ -0,0 +1,4 @@
+import SecondPage from "./second_page";
+
+export default SecondPage;
+export * from "./second_page";
---
+++ src/pages/signup/components/second_page/second_page.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import SignupForm from "../../signup_form";
+import SignupFormSet from "../../signup_form_set";
+import FormInput from "../../../../components/form_input/";
+import DateInput from "../../../../components/date_input";
+import Button, { ButtonStyle } from "../../../../components/button";
+
+import "./second_page.scss";
+
+export interface Props
+{
+    signupData: SignupForm;
+    changePage(page: number, data: SignupFormSet): void;
+}
+
+const SecondPage: React.FunctionComponent<Props> = (props) =>
+{
+    const [ enableNext, setEnableNext ] = useState(false);
+
+    return <div className="form-modal">
+        <header className="signup-form-top">
+            <div id="signup-close-button">
+                <i className="fa-solid fa-xmark"></i>
+            </div>
+        </header>
+
+        <section className="signup-form-body">
+            <h1>Termina el registro</h1>
+
+            <div className="form-elements">
+                <FormInput id="signup-handle" title="Nombre de usuario" value={props.signupData.handle} />
+
+                <FormInput id="signup-password" title="Contraseña" value={props.signupData.password} />
+
+                <FormInput id="signup-repassword" title="Reingresa la contraseña" value={props.signupData.repassword} />
+            </div>
+        </section>
+
+        <footer className="signup-form-bottom">
+            <div className="button-container">
+                <Button stylePreset={ButtonStyle.Blue} disabled={!enableNext}>
+                    Crear cuenta
+                </Button>
+            </div>
+        </footer>
+    </div>;
+};
+
+export default SecondPage;
