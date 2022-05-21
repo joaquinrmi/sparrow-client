1bf7f0d2e49cab9614abb493dd5e3be465b7691d
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-21 05:06:27 -0300

    Add error messages to SecondPage component

--- src/pages/signup/components/second_page/second_page.tsx
+++ src/pages/signup/components/second_page/second_page.tsx
@@ -13,9 +13,16 @@ export interface Props
     sendForm(data: SignupFormSet): Promise<void>;
 }
 
+const HANDLE_ERROR_MESSAGE = `El nombre de usuario solo puede contener letras minúsculas, números y el signo "_" y una longitud entre 4 y 15 caracteres.`;
+const PASSWORD_ERROR_MESSAGE = "La contraseña debe tener entre 8 y 20 caracteres de longitud.";
+const REPASSWORD_ERROR_MESSAGE = "Las contraseñas con coinciden.";
+
 const SecondPage: React.FunctionComponent<Props> = (props) =>
 {
     const [ enableButton, setEnableButton ] = useState(false);
+    const [ handleError, setHandleError ] = useState("");
+    const [ passwordError, setPasswordError ] = useState("");
+    const [ repasswordError, setRepasswordError ] = useState("");
 
     useEffect(() =>
     {
@@ -27,7 +34,7 @@ const SecondPage: React.FunctionComponent<Props> = (props) =>
 
         const checkEnableButton = () =>
         {
-            if(passwordInput.getValue().length < 8)
+            if(passwordInput.getValue().length < 8 || passwordInput.getValue().length > 20)
             {
                 setEnableButton(false);
                 return;
@@ -51,16 +58,52 @@ const SecondPage: React.FunctionComponent<Props> = (props) =>
         handleInput.onchange = () =>
         {
             checkEnableButton();
+
+            if(handleInput.getValue().length > 0 && !handleInput.getValue().match(handleRegex))
+            {
+                setHandleError(HANDLE_ERROR_MESSAGE);
+            }
+            else
+            {
+                setHandleError("");
+            }
         }
 
         passwordInput.onchange = () =>
         {
             checkEnableButton();
+
+            if(passwordInput.getValue().length < 8 || passwordInput.getValue().length > 20)
+            {
+                setPasswordError(PASSWORD_ERROR_MESSAGE);
+            }
+            else
+            {
+                setPasswordError("");
+            }
+
+            if(repasswordInput.getValue().length > 0 && passwordInput.getValue() !== repasswordInput.getValue())
+            {
+                setRepasswordError(REPASSWORD_ERROR_MESSAGE);
+            }
+            else
+            {
+                setRepasswordError("");
+            }
         }
 
         repasswordInput.onchange = () =>
         {
             checkEnableButton();
+
+            if(repasswordInput.getValue().length > 0 && passwordInput.getValue() !== repasswordInput.getValue())
+            {
+                setRepasswordError(REPASSWORD_ERROR_MESSAGE);
+            }
+            else
+            {
+                setRepasswordError("");
+            }
         }
     },
     []);
@@ -76,11 +119,11 @@ const SecondPage: React.FunctionComponent<Props> = (props) =>
             <h1>Termina el registro</h1>
 
             <div className="form-elements">
-                <FormInput id="signup-handle" title="Nombre de usuario" value={props.signupData.handle} />
+                <FormInput id="signup-handle" title="Nombre de usuario" value={props.signupData.handle} errorMessage={handleError} />
 
-                <FormInput id="signup-password" type={FormInputType.Password} title="Contraseña" value={props.signupData.password} />
+                <FormInput id="signup-password" type={FormInputType.Password} title="Contraseña" value={props.signupData.password} errorMessage={passwordError} />
 
-                <FormInput id="signup-repassword" type={FormInputType.Password} title="Reingresa la contraseña" value={props.signupData.repassword} />
+                <FormInput id="signup-repassword" type={FormInputType.Password} title="Reingresa la contraseña" value={props.signupData.repassword} errorMessage={repasswordError} />
             </div>
         </section>
 
