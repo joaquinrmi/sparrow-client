7caf6c12e6338c72a526d9521ec9320137c83b5a
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-25 04:49:28 -0300

    Add MainAside component

---
+++ src/pages/sparrow/components/main_aside/index.tsx
@@ -0,0 +1,4 @@
+import MainAside from "./main_aside";
+
+export default MainAside;
+export * from "./main_aside";
---
+++ src/pages/sparrow/components/main_aside/main_aside.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./main_aside.scss";
+
+const MainAside: React.FunctionComponent = (props) =>
+{
+    return <aside></aside>;
+};
+
+export default MainAside;
