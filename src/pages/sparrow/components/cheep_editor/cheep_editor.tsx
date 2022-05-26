f2f865f990f05c9944c5d2dd92a43baf551410af
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-26 06:34:22 -0300

    Add CheepEditor component

---
+++ src/pages/sparrow/components/cheep_editor/cheep_editor.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./cheep_editor.scss";
+
+export interface Props
+{}
+
+const CheepEditor: React.FunctionComponent<Props> = (props) =>
+{
+    return <div className="cheep-editor">
+        Editor de cheeps
+    </div>;
+};
+
+export default CheepEditor;
---
+++ src/pages/sparrow/components/cheep_editor/index.tsx
@@ -0,0 +1,4 @@
+import CheepEditor from "./cheep_editor";
+
+export default CheepEditor;
+export * from "./cheep_editor";
