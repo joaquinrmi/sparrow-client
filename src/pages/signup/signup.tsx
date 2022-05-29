870db8f87f087861521594952684c587e9ca7e3b
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-29 09:24:40 -0300

    Add sendSignupForm() function

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -3,7 +3,12 @@ import FirstPage from "./components/first_page";
 import SignupForm from "./signup_form";
 import SignupFormSet from "./signup_form_set";
 import SecondPage from "./components/second_page";
+import LoadingPage from "../loading_page";
+
+import login from "../../login";
 import SessionContext from "../../session_context";
+import CreateUserForm from "./create_user_form";
+import UserSession from "../../user_session";
 
 import "./signup.scss";
 
@@ -42,25 +47,32 @@ const Signup: React.FunctionComponent = () =>
         });
     };
 
+    const sendForm = async (data: SignupFormSet) =>
+    {        
+        setSate((currentState) =>
+        {
+            return {
+                sendForm: true,
+                currentPage: currentState.currentPage,
+                signupData: {
+                    ...currentState.signupData,
+                    ...data
+                }
+            };
+        });
+    };
+
     return <SessionContext.Consumer>{(session) =>
     {
-        const sendForm = async (data: SignupFormSet) =>
-        {        
-            setSate((currentState) =>
-            {
-                return {
-                    sendForm: true,
-                    currentPage: currentState.currentPage,
-                    signupData: {
-                        ...currentState.signupData,
-                        ...data
-                    }
-                };
-            });
-        };
-
         let content: any;
-        if(state.currentPage === 1)
+
+        if(state.sendForm)
+        {
+            sendSignupForm(state.signupData, session);
+
+            return <LoadingPage />;
+        }
+        else if(state.currentPage === 1)
         {
             content = <FirstPage signupData={state.signupData} changePage={changePage} />;
         }
@@ -76,6 +88,61 @@ const Signup: React.FunctionComponent = () =>
         </div>;
     }}</SessionContext.Consumer>
 };
+
+async function sendSignupForm(signupData: SignupForm, session: UserSession)
+{
+    const form: CreateUserForm = {
+        handle: signupData.handle,
+        email: signupData.email,
+        password: signupData.password,
+        fullName: signupData.name,
+        birthdate: signupData.birthdate.getTime(),
+    };
+
+    if(process.env.REACT_APP_SERVER)
+    {
+        const signupURL = `${process.env.REACT_APP_SERVER}/api/user/create`;
+
+        const response = await fetch(signupURL,
+        {
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form),
+            credentials: "include"
+        });
+
+        if(response.status === 201)
+        {
+            const { handle } = await response.json() as { handle: string };
+
+            try
+            {
+                var userData = await login({
+                    handleOrEmail: handle,
+                    password: form.password,
+                    remember: true
+                });
+            }
+            catch(err)
+            {
+                return console.error(err);
+            }
+
+            session.login(userData);
+        }
+        else
+        {
+            console.error(await response.json());
+        }
+    }
+    else
+    {
+        console.error("The server URL is not defined.");
+    }
+}
 
 interface SignupState
 {
