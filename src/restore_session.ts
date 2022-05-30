dcb0c2f877770eb53527f0e13a592e4bee98d549
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-30 08:56:25 -0300

    Add restoreSession() function

---
+++ src/restore_session.ts
@@ -0,0 +1,38 @@
+import UserData from "./user_data";
+
+async function restoreSession(): Promise<UserData>
+{
+    if(process.env.REACT_APP_SERVER)
+    {
+        const restoreSessionURL = `${process.env.REACT_APP_SERVER}/api/user/restore-session`;
+
+        const response = await fetch(restoreSessionURL, {
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json"
+            },
+        });
+
+        if(response.status === 200)
+        {
+            const data = await response.json();
+
+            return {
+                handle: data.handle,
+                name: data.name,
+                picture: data.picture
+            };
+        }
+        else
+        {
+            throw await response.json();
+        }
+    }
+    else
+    {
+        throw new Error("The server URL is not defined.");
+    }
+}
+
+export default restoreSession;
