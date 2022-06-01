6de740d56be55140bfe0134e9ea1bc67ac105e02
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-01 15:04:00 -0300

    Add StateManager class

---
+++ src/pages/sparrow/state_manager.ts
@@ -0,0 +1,34 @@
+import React from "react";
+import CheepData from "../../cheep_data";
+import SearchCheepsQuery from "../../search_cheep_query";
+import SparrowState, { CheepListName } from "./state";
+
+class StateManager
+{
+    private setState?: React.Dispatch<React.SetStateAction<SparrowState>>;
+
+    constructor(setState?: React.Dispatch<React.SetStateAction<SparrowState>>)
+    {
+        this.setState = setState;
+    }
+
+    loadCheepList(listName: CheepListName, query: SearchCheepsQuery, cheeps: Array<CheepData>): void
+    {
+        if(this.setState)
+        {
+            this.setState((state) =>
+            {
+                const newState = { ...state };
+
+                newState.cheepLists[listName] = {
+                    query: query,
+                    cheeps: cheeps
+                };
+
+                return newState;
+            });
+        }
+    }
+}
+
+export default StateManager;
