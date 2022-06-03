0b2c121b7cf8b001456184fa7ba6e0628d6c9b7f
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-03 13:31:32 -0300

    Add 'height' to ModalForm Props

--- src/components/modal_form/modal_form.tsx
+++ src/components/modal_form/modal_form.tsx
@@ -5,11 +5,14 @@ import "./modal_form.scss";
 export interface Props
 {
     children?: React.ReactNode;
+    height: string;
 }
 
 const ModalForm: React.FunctionComponent<Props> = (props) =>
 {
-    return <div className="modal-form">
+    return <div className="modal-form" style={{
+        flex: `0 1 ${props.height}`
+    }}>
         {props.children}
     </div>;
 };
