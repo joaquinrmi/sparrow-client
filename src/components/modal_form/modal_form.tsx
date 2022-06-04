dd3c4f3ae3b00939349c614b1ed2393c02ba94f6
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-04 15:51:57 -0300

    Add 'className' to ModalForm component

--- src/components/modal_form/modal_form.tsx
+++ src/components/modal_form/modal_form.tsx
@@ -5,14 +5,12 @@ import "./modal_form.scss";
 export interface Props
 {
     children?: React.ReactNode;
-    height: string;
+    className?: string;
 }
 
 const ModalForm: React.FunctionComponent<Props> = (props) =>
 {
-    return <div className="modal-form" style={{
-        flex: `0 1 ${props.height}`
-    }}>
+    return <div className={`modal-form ${props.className ? props.className : ""}`}>
         {props.children}
     </div>;
 };
