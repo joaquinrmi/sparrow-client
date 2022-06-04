f9318ba9536f161987aa54a6a95f1d3fcf18f9e0
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-04 11:48:41 -0300

    Improve Loading component

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -3,16 +3,35 @@ import React from "react";
 import "./loading.scss";
 
 export interface Props
-{}
+{
+    static?: boolean;
+    status?: number;
+    radius?: number;
+    thickness?: number;
+}
+
+const RADIUS = 12;
 
 const BLUE = "#00ACF4";
 const LIGHT_BLUE = "#D2EBFC";
 
 const Loading: React.FunctionComponent<Props> = (props) =>
 {
-    return <svg className="loading" width="30" height="30">
-        <circle cx="50%" cy="50%" r="12" strokeWidth="4" stroke={LIGHT_BLUE} fill="none"></circle>
-        <circle className="animated-circle" cx="50%" cy="50%" r="12" strokeWidth="4" stroke={BLUE} fill="none"></circle>
+    const radius = props.radius ? props.radius : RADIUS;
+    const scale = radius / RADIUS;
+    const thickness = (props.thickness ? props.thickness : 4) * (1 / scale);
+    const size = (2 * RADIUS) + thickness + 2;
+    const length = 2 * Math.PI * RADIUS;
+    const offset = length - (props.status ? props.status * length / 100 : 0);
+
+    return <svg className={`loading ${props.static ? "static" : "animated"}`} width={size} height={size} style={{
+        transform: `scale(${scale}, ${scale})`
+    }}>
+        <circle cx="50%" cy="50%" r={RADIUS} strokeWidth={thickness} stroke={LIGHT_BLUE} fill="none"></circle>
+
+        <circle className="status-circle" cx="50%" cy="50%" r={RADIUS} strokeWidth={thickness} stroke={BLUE} fill="none" style={props.static ? {
+            strokeDashoffset: `${offset}px`
+        } : {}}></circle>
     </svg>;
 };
 
