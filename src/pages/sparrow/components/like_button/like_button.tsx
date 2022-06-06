091ea42a78ea2753bd24c969afa0eac8696b4c4a
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-06 20:53:40 -0300

    Register cheep like

--- src/pages/sparrow/components/like_button/like_button.tsx
+++ src/pages/sparrow/components/like_button/like_button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React  from "react";
 import InteractionButton, { InteractionColor } from "../interaction_button";
 
 export interface Props
@@ -6,13 +6,27 @@ export interface Props
     cheepId: number;
     counter?: number;
     active?: boolean;
+
+    onClick?(): void;
 }
 
 const LikeButton: React.FunctionComponent<Props> = (props) =>
 {
-    return <InteractionButton color={InteractionColor.Pink} icon="heart" counter={props.counter} active={props.active} onClick={(event) =>
+    return <InteractionButton color={InteractionColor.Pink} icon="heart" counter={props.counter} active={props.active} onClick={async (event) =>
     {
         event.stopPropagation();
+
+        if(props.onClick)
+        {
+            props.onClick();
+        }
+
+        const likeURL = `${process.env.REACT_APP_SERVER}/api/cheep/like?cheepId=${props.cheepId}`;
+
+        await fetch(likeURL, {
+            method: "POST",
+            credentials: "include"
+        });
     }} />;
 };
 
