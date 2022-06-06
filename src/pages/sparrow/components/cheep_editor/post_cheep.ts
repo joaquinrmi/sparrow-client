0da81c82030f0d87f1295faf5a5652d76f5ea1a9
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-06 01:36:29 -0300

    Add 'postCheep()' function

---
+++ src/pages/sparrow/components/cheep_editor/post_cheep.ts
@@ -0,0 +1,26 @@
+import CreateCheepData from "./create_cheep_data";
+
+async function postCheep(data: CreateCheepData): Promise<number>
+{
+    const postCheepURL = `${process.env.REACT_APP_SERVER}/api/cheep/create`;
+
+    const response = await fetch(postCheepURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+        credentials: "include"
+    });
+
+    if(response.status === 201)
+    {
+        return await response.json();
+    }
+    else
+    {
+        throw await response.json();
+    }
+}
+
+export default postCheep;
