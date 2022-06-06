53ebc1ace106a74dc76af3f63b7bdabd21625f7a
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-06 20:07:15 -0300

    Add ResponseErrorType enum

---
+++ src/response_error_type.ts
@@ -0,0 +1,16 @@
+enum ResponseErrorType
+{
+    InternalServerError = "Internal Server Error",
+    InvalidQuery = "Invalid Query",
+    InvalidForm = "Invalid Form",
+    HandleAlreadyExists = "Username Already Exists",
+    EmailAlreadyExists = "Email Already Exists",
+    ProfileNotFound = "Profile Not Found",
+    IncorrectUsernameOrPassword = "Incorrect Username Or Password",
+    SessionDoesNotExist = "Session Does Not Exist",
+    InvalidCheepContent = "Invalid Cheep Content",
+    CheepDoesNotExist = "Cheep Does Not Exist",
+    CannotDeleteCheep = "Cannot Delete Cheep"
+}
+
+export default ResponseErrorType;
