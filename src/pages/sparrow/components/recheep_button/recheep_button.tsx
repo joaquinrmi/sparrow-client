1ccd98f1aba610984dc01fc6cf7fd9d73e97ec1a
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-07 19:01:24 -0300

    Add 'cheepData' to RecheepButton Props

--- src/pages/sparrow/components/recheep_button/recheep_button.tsx
+++ src/pages/sparrow/components/recheep_button/recheep_button.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import CheepData from "../../../../cheep_data";
+import StateContext from "../../state_context";
 import InteractionButton, { InteractionColor } from "../interaction_button";
 
 export interface Props
 {
-    cheepId: number;
+    cheepData: CheepData;
     counter?: number;
     active?: boolean;
 }
 
 const RecheepButton: React.FunctionComponent<Props> = (props) =>
 {
+    const [ state, stateManager ] = useContext(StateContext);
+    const navigate = useNavigate();
+    
     return <InteractionButton color={InteractionColor.Green} icon="retweet" counter={props.counter} active={props.active} onClick={(event) =>
     {
         event.stopPropagation();
+
+        stateManager.setEditorTargetCheep(props.cheepData);
+        navigate("/compose/cheep");
     }} />;
 };
 
