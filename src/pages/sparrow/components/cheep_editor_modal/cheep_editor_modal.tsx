8af60aba2dfe5423429506e56c95c7692ee8b245
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-06 22:17:39 -0300

    Add 'onClose()' to CheepEditorModal Props

--- src/pages/sparrow/components/cheep_editor_modal/cheep_editor_modal.tsx
+++ src/pages/sparrow/components/cheep_editor_modal/cheep_editor_modal.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Modal from "../../../../components/modal";
 import CheepEditor from "../cheep_editor/cheep_editor";
 
 import "./cheep_editor_modal.scss";
 
-const CheepEditorModal: React.FunctionComponent = () =>
+export interface Props
+{
+    onClose?(): void;
+}
+
+const CheepEditorModal: React.FunctionComponent<Props> = (props) =>
 {
     const navigate = useNavigate();
+
+    useEffect(() =>
+    {
+        return () =>
+        {
+            if(props.onClose)
+            {
+                props.onClose();
+            }
+        };
+    });
 
     return <Modal id="compose-modal" className="cheep-editor-modal" closeRequest={() =>
     {
