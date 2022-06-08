bad9aea0af021c5a3f847ff4a8e6b8a7b4c08eeb
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-08 18:15:56 -0300

    Add 'onRecheep()' to RecheepButton Props

--- src/pages/sparrow/components/recheep_button/recheep_button.tsx
+++ src/pages/sparrow/components/recheep_button/recheep_button.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import CheepData from "../../../../cheep_data";
 import StateContext from "../../state_context";
 import InteractionButton, { InteractionColor } from "../interaction_button";
@@ -10,19 +9,31 @@ export interface Props
     cheepData: CheepData;
     counter?: number;
     active?: boolean;
+
+    onRecheep?(): void;
 }
 
 const RecheepButton: React.FunctionComponent<Props> = (props) =>
 {
     const [ state, stateManager ] = useContext(StateContext);
-    const navigate = useNavigate();
     
     return <InteractionButton id={props.id} color={InteractionColor.Green} icon="retweet" counter={props.counter} active={props.active} onClick={(event) =>
     {
         event.stopPropagation();
 
-        stateManager.setEditorTargetCheep(props.cheepData);
-        navigate("/compose/cheep");
+        const button = document.getElementById(props.id) as HTMLDivElement;
+
+        const rect = button.getBoundingClientRect();
+        const x = rect.x + window.scrollX;
+        const y = rect.y + window.scrollY;
+
+        stateManager.openRecheepMenu(props.cheepData, [ x, y ], () =>
+        {
+            if(props.onRecheep)
+            {
+                props.onRecheep();
+            }
+        });
     }} />;
 };
 
