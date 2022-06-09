debb328b0036463fd0df58a04cdf3cfe21c08f17
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-09 15:38:59 -0300

    Add 'deleteCheep()' function

---
+++ src/pages/sparrow/components/cheep_editor/delete_cheep.ts
@@ -0,0 +1,18 @@
+async function deleteCheep(cheepId: number): Promise<boolean>
+{
+    const deleteCheepURL = `${process.env.REACT_APP_SERVER}/api/cheep/delete?cheepId=${cheepId}`;
+
+    const response = await fetch(deleteCheepURL, {
+        method: "POST",
+        credentials: "include"
+    });
+
+    if(response.status === 200)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+export default deleteCheep;
