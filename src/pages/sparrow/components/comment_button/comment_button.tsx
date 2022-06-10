05ec69df4e27900e5459a9ed4ba59df4f453949e
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-09 22:36:44 -0300

    Open EditorCheepModal on CommentButton click

--- src/pages/sparrow/components/comment_button/comment_button.tsx
+++ src/pages/sparrow/components/comment_button/comment_button.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import CheepData from "../../../../cheep_data";
+import StateContext from "../../state_context";
 import InteractionButton, { InteractionColor } from "../interaction_button";
 
 export interface Props
@@ -11,9 +13,16 @@ export interface Props
 
 const CommentButton: React.FunctionComponent<Props> = (props) =>
 {
+    const [ state, stateManager ] = useContext(StateContext);
+
+    const navigate = useNavigate();
+
     return <InteractionButton id={props.id} color={InteractionColor.Blue} icon="comment" counter={props.counter ? props.cheepData.commentCount : undefined} onClick={(event) =>
     {
         event.stopPropagation();
+
+        stateManager.setEditorResponseTarget(props.cheepData);
+        navigate("/compose/cheep");
     }} />;
 };
 
