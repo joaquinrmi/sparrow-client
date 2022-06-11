df991978cbdf5f068937c37a1a59ebb49a9b744b
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-11 02:44:33 -0300

    Add Thread component

---
+++ src/components/thread/thread.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { CheepListName } from "../../pages/sparrow/state";
+import StateContext from "../../pages/sparrow/state_context";
+import Cheep from "../cheep";
+
+export interface Props
+{
+    name: CheepListName;
+}
+
+const Thread: React.FunctionComponent<Props> = (props) =>
+{
+    const [ state, stateManager ] = useContext(StateContext);
+
+    const cheepList = state.cheepLists[props.name];
+
+    return <>
+        {cheepList.cheeps.map((cheepData, index) =>
+        {
+            return <Cheep key={`${index}-cheep`} id={`${index}-cheep-${props.name}`} data={cheepData} index={index} listName={props.name} />;
+        })}
+    </>;
+}
+
+export default Thread;
