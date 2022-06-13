6d924c9a2bee38d0f67162aba029c3591e04acdb
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-13 00:32:42 -0300

    Add close behaviour to ProfileFormModal component

--- src/pages/sparrow/components/profile_form_modal/profile_form_modal.tsx
+++ src/pages/sparrow/components/profile_form_modal/profile_form_modal.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Modal from "../../../../components/modal";
+import StateContext from "../../state_context";
 import ProfileForm from "../profile_form/profile_form";
 
 import "./profile_form_modal.scss";
@@ -9,9 +11,31 @@ export interface Props
 
 const ProfileFormModal: React.FunctionComponent<Props> = (props) =>
 {
+    const [ hasChanged, setHasChanged ] = useState<boolean>(false);
+
+    const [ state, stateManager ] = useContext(StateContext);
+
+    const navigate = useNavigate();
+
     return <Modal id="profile-form-modal" className="profile-form-modal" closeRequest={() =>
-    {}}>
-        <ProfileForm />
+    {
+        if(hasChanged)
+        {
+            stateManager.openCloseConfirmation(() =>
+            {
+                stateManager.closeCloseConfirmation();
+                navigate(-1);
+            });
+        }
+        else
+        {
+            navigate(-1);
+        }
+    }}>
+        <ProfileForm somethingHasBeenTouched={() =>
+        {
+            setHasChanged(true);
+        }} />
     </Modal>;
 };
 
