ea283e59e2b8ebd4df86712d07d609faff7820ec
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-13 03:41:07 -0300

    Add ProfileForm interface

---
+++ src/pages/sparrow/components/profile_form/profile_form.ts
@@ -0,0 +1,11 @@
+interface ProfileForm
+{
+    banner?: File;
+    picture?: File;
+    name?: string;
+    description?: string;
+    location?: string;
+    website?: string;
+}
+
+export default ProfileForm;
