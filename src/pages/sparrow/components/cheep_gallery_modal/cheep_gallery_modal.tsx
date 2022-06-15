19117c6f0901f7bbd3f9eb71b1b9f634a69c486e
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-14 21:55:04 -0300

    Add collapse button to CheepGalleryModal component

--- src/pages/sparrow/components/cheep_gallery_modal/cheep_gallery_modal.tsx
+++ src/pages/sparrow/components/cheep_gallery_modal/cheep_gallery_modal.tsx
@@ -108,10 +108,35 @@ const CheepGalleryModal: React.FunctionComponent<Props> = (props) =>
             }
         };
 
+        const asideElement = document.querySelector(".cheep-modal-container .cheep-container") as HTMLDivElement;
+        const iElement = document.querySelector(".gallery-button.collapse i") as HTMLSpanElement;
+
+        const onResize = () =>
+        {
+            if(asideElement !== null)
+            {
+                if(iElement !== null)
+                {
+                    if(asideElement.getBoundingClientRect().width === 0)
+                    {
+                        iElement.classList.remove("fa-angles-right");
+                        iElement.classList.add("fa-angles-left");
+                    }
+                    else
+                    {
+                        iElement.classList.remove("fa-angles-left");
+                        iElement.classList.add("fa-angles-right");
+                    }
+                }
+            }
+        }
+
+        window.addEventListener("resize", onResize);
         document.addEventListener("keydown", navigateOnGallery);
 
         return () =>
         {
+            window.removeEventListener("resize", onResize);
             document.removeEventListener("keydown", navigateOnGallery);
         }
     });
@@ -158,6 +183,50 @@ const CheepGalleryModal: React.FunctionComponent<Props> = (props) =>
                         navigate(-navigationCount);
                     }}>
                         <i className="fa-solid fa-xmark"></i>
+                    </div>
+
+                    <div className="gallery-button collapse" onClick={(ev) =>
+                    {
+                        const asideElement = document.querySelector(".cheep-modal-container .cheep-container") as HTMLDivElement;
+
+                        if(asideElement === null)
+                        {
+                            return;
+                        }
+
+                        if(asideElement.classList.contains("hidden"))
+                        {
+                            asideElement.classList.remove("hidden");
+                            if(window.innerWidth < 950)
+                            {
+                                asideElement.classList.add("show");
+                            }
+                        }
+                        else
+                        {
+                            asideElement.classList.remove("show");
+                            asideElement.classList.add("hidden");
+                        }
+
+                        const iElement = document.querySelector(".gallery-button.collapse i") as HTMLSpanElement;
+
+                        if(iElement === null)
+                        {
+                            return;
+                        }
+
+                        if(asideElement.getBoundingClientRect().width === 0)
+                        {
+                            iElement.classList.remove("fa-angles-right");
+                            iElement.classList.add("fa-angles-left");
+                        }
+                        else
+                        {
+                            iElement.classList.remove("fa-angles-left");
+                            iElement.classList.add("fa-angles-right");
+                        }
+                    }}>
+                        <i className="fa-solid fa-angles-right"></i>
                     </div>
 
                     {props.photoIndex > 1 ?
