3fd361c1d03c6f55c5069224e0f487bf5f98cd1a
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-16 07:38:16 -0300

    Use PageNavigation in ProfileNavigation component

--- src/pages/sparrow/components/profile_navigation/profile_navigation.tsx
+++ src/pages/sparrow/components/profile_navigation/profile_navigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useResolvedPath, useMatch } from "react-router-dom";
+import PageNavigation from "../page_navigation";
 
 import "./profile_navigation.scss";
 
@@ -10,36 +10,24 @@ export interface Props
 
 const ProfileNavigation: React.FunctionComponent<Props> = (props) =>
 {
-    return <div className="profile-navigation">
-        <div className="items-wrapper">
-            <NavigationItem to={`/${props.userHandle}`}>Cheeps</NavigationItem>
-            <NavigationItem to={`/${props.userHandle}/with-replies`}>Cheeps y respuestas</NavigationItem>
-            <NavigationItem to={`/${props.userHandle}/media`}>Fotos y videos</NavigationItem>
-            <NavigationItem to={`/${props.userHandle}/likes`}>Me gusta</NavigationItem>
-        </div>
-    </div>;
-};
-
-interface NavigationItemProps
-{
-    children?: React.ReactNode;
-    to: string;
-}
-
-const NavigationItem: React.FunctionComponent<NavigationItemProps> = (props) =>
-{
-    let resolved = useResolvedPath(props.to);
-    let match = useMatch({ path: resolved.pathname, end: true });
-
-    return <div className="nav-item">
-        <Link to={props.to} className={`nav-button ${match ? "active" : ""}`}>
-            <span className="nav-text">
-                {props.children}
-                
-                <div className="color-bar"></div>
-            </span>
-        </Link>
-    </div>;
+    return <PageNavigation items={[
+        {
+            to: `/${props.userHandle}`,
+            content: "Cheeps"
+        },
+        {
+            to: `/${props.userHandle}/with-replies`,
+            content: "Cheeps y respuestas"
+        },
+        {
+            to: `/${props.userHandle}/media`,
+            content: "Fotos y videos"
+        },
+        {
+            to: `/${props.userHandle}/likes`,
+            content: "Me gusta"
+        }
+    ]} />
 };
 
 export default ProfileNavigation;
