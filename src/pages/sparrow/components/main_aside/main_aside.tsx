34564369d005a444186786b4d058c478149c14c5
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-16 03:18:27 -0300

    Add links to MainAside component

--- src/pages/sparrow/components/main_aside/main_aside.tsx
+++ src/pages/sparrow/components/main_aside/main_aside.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import SearchBar from "./components/search_bar";
 
 import "./main_aside.scss";
 
 const MainAside: React.FunctionComponent = (props) =>
 {
-    return <aside className="main-aside"></aside>;
+    return <aside className="main-aside">
+        <SearchBar id="main-search-bar" />
+
+        <div className="links-container">
+            <ul>
+                <li><a href="https://joaquinrmi.github.io/porfolio/">Porfolio</a></li>
+                <li><a href="https://www.linkedin.com/in/joaquin-ruaimi-3381a1201/">LinkedIn</a></li>
+                <li><a href="https://github.com/joaquinrmi">Github</a></li>
+                <li><a href="https://github.com/joaquinrmi/sparrow-client">Repositorio (front-end)</a></li>
+                <li><a href="https://github.com/joaquinrmi/sparrow-server">Repositorio (back-end)</a></li>
+            </ul>
+        </div>
+    </aside>;
 };
 
 export default MainAside;
