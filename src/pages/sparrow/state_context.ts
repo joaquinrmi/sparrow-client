c7d0a697d96a31f6aaf93b2b7163d3de82af992c
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-16 08:11:58 -0300

    Fix error in StateContext

--- src/pages/sparrow/state_context.ts
+++ src/pages/sparrow/state_context.ts
@@ -7,6 +7,9 @@ const StateContext = createContext<[ SparrowState, StateManager ]>([
         location: {
             profile: {
                 currentRoute: ""
+            },
+            innerProfile: {
+                currentRoute: ""
             }
         },
         profile: {
