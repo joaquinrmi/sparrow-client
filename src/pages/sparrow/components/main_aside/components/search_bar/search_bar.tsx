927a2012b01c76b4e616ac2f4ac1ec2ee19df4a3
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-16 04:18:24 -0300

    Search on enter key down

--- src/pages/sparrow/components/main_aside/components/search_bar/search_bar.tsx
+++ src/pages/sparrow/components/main_aside/components/search_bar/search_bar.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./search_bar.scss";
 
@@ -10,6 +11,8 @@ export interface Props
 
 const SearchBar: React.FunctionComponent<Props> = (props) =>
 {
+    const navigate = useNavigate();
+
     useEffect(() =>
     {
         const element = document.getElementById(props.id) as HTMLDivElement;
@@ -52,7 +55,30 @@ const SearchBar: React.FunctionComponent<Props> = (props) =>
     },
     [ props.id ]);
 
-    return <div id={props.id} className="search-bar-container">
+    return <div id={props.id} className="search-bar-container" onKeyDown={(ev) =>
+    {
+        const element = document.getElementById(props.id) as HTMLDivElement;
+        if(element === null)
+        {
+            return;
+        }
+
+        const inputElement = element.querySelector("input") as HTMLInputElement;
+        if(inputElement === null)
+        {
+            return;
+        }
+
+        if(inputElement.value.trim().length === 0)
+        {
+            return;
+        }
+
+        if(ev.key === "Enter")
+        {
+            navigate(`/search?q=${inputElement.value}`);
+        }
+    }}>
         <div className="icon">
             <i className="fa-solid fa-magnifying-glass"></i>
         </div>
