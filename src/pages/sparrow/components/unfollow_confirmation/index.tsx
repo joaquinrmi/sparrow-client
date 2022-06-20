3e66ad720129abfb43226f1c37e1a5e87caa4538
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-19 21:33:38 -0300

    Add UnfollowConfirmation component

---
+++ src/pages/sparrow/components/unfollow_confirmation/index.tsx
@@ -0,0 +1,4 @@
+import UnfollowConfirmation from "./unfollow_confirmation";
+
+export default UnfollowConfirmation;
+export * from "./unfollow_confirmation";
---
+++ src/pages/sparrow/components/unfollow_confirmation/unfollow_confirmation.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import StateContext from "../../state_context";
+import ConfirmationModal, { ConfirmationStyle } from "../confirmation_modal";
+
+export interface Props
+{
+    userHandle: string;
+}
+
+const UnfollowConfirmation: React.FunctionComponent<Props> = (props) =>
+{
+    const [ state, stateManager ] = useContext(StateContext);
+
+    return <ConfirmationModal
+        id="close-confirmation-modal"
+        title={`¿Quieres dejar de seguir a @${props.userHandle}?`}
+        message="Sus Tweets ya no aparecerán en tu cronología de inicio. Podrás seguir viendo su perfil, a menos que sus Tweets estén protegidos."
+        styleType={ConfirmationStyle.Normal}
+        confirmButtonMessage="Dejar de seguir"
+        cancelButtonMessage="Cancelar"
+        confirm={state.closeConfirmation.discart}
+        cancel={stateManager.closeCloseConfirmation}
+        closeRequest={stateManager.closeCloseConfirmation}
+    />;
+};
+
+export default UnfollowConfirmation;
