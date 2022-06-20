0a7646c815701fce75463f059d5392040f8f99f4
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-20 05:44:09 -0300

    Fix error in followUser() function

--- src/pages/sparrow/follow_user.ts
+++ src/pages/sparrow/follow_user.ts
@@ -1,9 +1,15 @@
 async function followUser(userHandle: string): Promise<boolean>
 {
-    const followURL = `${process.env.REACT_APP_SERVER}/api/user/follow?userHandle=${userHandle}`;
+    const followURL = `${process.env.REACT_APP_SERVER}/api/user/follow`;
 
     const response = await fetch(followURL, {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            userHandle
+        }),
         credentials: "include"
     });
 
