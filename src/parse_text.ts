485aee198b8c84bcb0a94df131cd8cf91618ce93
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-20 10:22:53 -0300

    Add 'URL' to TokenType and improve the text parser

--- src/parse_text.ts
+++ src/parse_text.ts
@@ -1,4 +1,20 @@
-const searchHashtag = /\s#[a-zA-Z0-9_]+|^#[a-zA-Z0-9_]+/g;
+const REG = /(\s*#[a-zA-Z0-9_]+)|((https?:\/\/)?(www\.)?[a-zA-Z0-9\-_.]{1,256}\.[a-zA-Z0-9]{2,6}([#?/][a-zA-Z0-9#?=\[\],()/]*)?)/g;
+
+const HASHTAG_START_REG = /\s*#/;
+
+export enum TokenType
+{
+    Plain,
+    Hashtag,
+    URL
+}
+ 
+export interface Token
+{
+    type: TokenType;
+    value: string;
+}
+
 
 function parseText(text: string): Array<Token>
 {
@@ -9,7 +25,7 @@ function parseText(text: string): Array<Token>
     
     let result = new Array<Token>();
     
-    const found = [ ...text.matchAll(searchHashtag) ];
+    const found = [ ...text.matchAll(REG) ];
 
     let lastIndex = 0;
     for(let i = 0; i < found.length; ++i)
@@ -21,15 +37,27 @@ function parseText(text: string): Array<Token>
         }
         
         let start = element.index;
+
+        let type: TokenType;
         let toAdd: string;
 
-        if(element[0].charCodeAt(0) === 32)
+        if(HASHTAG_START_REG.test(element[0]))
         {
-            start += 1;
-            toAdd = element[0].substring(1);
+            type = TokenType.Hashtag;
+
+            if(element[0].charCodeAt(0) === 32)
+            {
+                start += 1;
+                toAdd = element[0].substring(1);
+            }
+            else
+            {
+                toAdd = element[0];
+            }
         }
         else
         {
+            type = TokenType.URL;
             toAdd = element[0];
         }
 
@@ -39,7 +67,7 @@ function parseText(text: string): Array<Token>
         });
 
         result.push({
-            type: TokenType.Hashtag,
+            type: type,
             value: toAdd
         });
 
@@ -53,38 +81,4 @@ function parseText(text: string): Array<Token>
 
     return result;
 }
-
-export enum TokenType
-{
-    Plain,
-    Hashtag
-}
- 
-export interface Token
-{
-    type: TokenType;
-    value: string;
-}
-
-export function tokensToHTML(tokens: Array<Token>): string
-{
-    let result = "";
-
-    for(const token of tokens)
-    {
-        switch(token.type)
-        {
-        case TokenType.Plain:
-            result += token.value;
-            break;
-
-        case TokenType.Hashtag:
-            result += `<span class="hashtag">${token.value}</span>`;
-            break;
-        }
-    }
-
-    return result;
-}
-
 export default parseText;
