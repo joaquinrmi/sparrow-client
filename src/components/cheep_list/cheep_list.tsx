300269b355b35b774df27fdc9da0411061aa7f9d
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 12:29:43 -0300

    Use 'loadCheeps()' in CheepList component

--- src/components/cheep_list/cheep_list.tsx
+++ src/components/cheep_list/cheep_list.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useContext } from "react";
 import Cheep from "../cheep/";
 import Loading from "../loading";
+import loadCheeps from "../../cheep/load_cheeps";
 
 import SearchCheepsQuery from "../../search_cheep_query";
-import CheepData from "../../cheep_data";
 import StateContext from "../../pages/sparrow/state_context";
 import { CheepListName } from "../../pages/sparrow/state";
-import processCheep from "../../cheep/process_cheep";
 
 import "./cheep_list.scss";
 
@@ -138,64 +137,6 @@ const CheepList: React.FunctionComponent<Props> = (props) =>
         }
     </div>;
 };
-
-async function loadCheeps(query: SearchCheepsQuery, hideResponseTarget?: boolean): Promise<{ cheeps: Array<CheepData>, nextTime: number }>
-{
-    const searchCheepsURL = `${process.env.REACT_APP_SERVER}/api/cheep/search${parseCheepQuery(query)}`;
-
-    const response = await fetch(searchCheepsURL, {
-        method: "GET",
-        credentials: "include"
-    });
-
-    if(response.status === 200)
-    {
-        const { cheeps, nextTime } = (await response.json()) as { cheeps: Array<any>, nextTime: number };
-
-        let processedCheeps = new Array<CheepData>();
-        for(let i = 0; i < cheeps.length; ++i)
-        {
-            const cheep = processCheep(cheeps[i]);
-
-            if(!hideResponseTarget && cheeps[i].responseOf !== undefined)
-            {
-                const responseTarget = processCheep(cheeps[i].responseOf);
-                responseTarget.existsJustBecauseItIsAResponseTarget = true;
-                cheep.responseOf = responseTarget;
-
-                processedCheeps.push(responseTarget);
-            }
-
-            processedCheeps.push(cheep);
-        }
-
-        return {
-            cheeps: processedCheeps,
-            nextTime: nextTime
-        };
-    }
-    else
-    {
-        throw new Error();
-    }
-}
-
-function parseCheepQuery(query: SearchCheepsQuery): string
-{
-    let elements = new Array<string>();
-
-    for(const arg in query)
-    {
-        elements.push(`${arg}=${query[arg as keyof SearchCheepsQuery]}`);
-    }
-
-    if(elements.length > 0)
-    {
-        return "?" + elements.join("&");
-    }
-
-    return "";
-}
 
 function compareQuery(first: SearchCheepsQuery, second: SearchCheepsQuery): boolean
 {
