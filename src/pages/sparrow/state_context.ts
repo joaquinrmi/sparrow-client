4832de8056e488c1e3be40c4d401f0710a7e3274
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 15:47:51 -0300

    Update default state value in StateContext

--- src/pages/sparrow/state_context.ts
+++ src/pages/sparrow/state_context.ts
@@ -41,6 +41,7 @@ const StateContext = createContext<[ SparrowState, StateManager ]>([
             comments: { query: {}, nextTime: 0, cheeps: [] },
             search: { query: {}, nextTime: 0, cheeps: [] },
             quotes: { query: {}, nextTime: 0, cheeps: [] },
+            userGallery: { query: {}, nextTime: 0, cheeps: [] },
         },
         userLists: {
             following: { id: "", target: "", users: [] },
