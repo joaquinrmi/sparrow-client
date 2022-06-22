8b197c93813f35d52330f0453e07ac759272670a
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 10:49:46 -0300

    Fix recheep count in Cheep component

--- src/components/cheep/cheep.tsx
+++ src/components/cheep/cheep.tsx
@@ -185,7 +185,7 @@ const Cheep: React.FunctionComponent<Props> = (props) =>
                             </div>
                             
                             <div className="interaction-button-container">
-                                <RecheepButton id={`recheep-${props.id}`} cheepData={props.data} active={recheep} counter={cheepData.recheepCount} onRecheep={() =>
+                                <RecheepButton id={`recheep-${props.id}`} cheepData={props.data} active={recheep} counter={cheepData.recheepCount + cheepData.withCommentsCount} onRecheep={() =>
                                 {
                                     let targetCheepData = { ...props.data };
                                     if(props.data === cheepData)
