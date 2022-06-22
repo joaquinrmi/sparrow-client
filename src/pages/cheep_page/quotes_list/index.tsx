189f236b8640b57d81729469a4e8f5d1658b09a2
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 07:38:30 -0300

    Add QuotesList component

---
+++ src/pages/cheep_page/quotes_list/index.tsx
@@ -0,0 +1,4 @@
+import QuotesList from "./quotes_list";
+
+export default QuotesList;
+export * from "./quotes_list";
---
+++ src/pages/cheep_page/quotes_list/quotes_list.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import CheepList from "../../../components/cheep_list";
+import PageHeader from "../../../components/page_header";
+
+export interface Props
+{
+    cheepId: number;
+}
+
+const QuotesList: React.FunctionComponent<Props> = (props) =>
+{
+    return <div className="likes-list">
+        <PageHeader>
+            <div className="title">
+                Cheeps citados
+            </div>
+        </PageHeader>
+
+        <CheepList name="quotes" arguments={{
+            quoteTarget: props.cheepId
+        }} hideResponseTarget />
+    </div>;
+};
+
+export default QuotesList;
