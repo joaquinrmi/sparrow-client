84bb33658b9488a6cb9adab3cf57e5444895a2c7
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 07:32:28 -0300

    Add 'quotes' to SparrowState.cheepLists

--- src/pages/sparrow/state.ts
+++ src/pages/sparrow/state.ts
@@ -24,6 +24,7 @@ interface SparrowState
         thread: CheepListState;
         comments: CheepListState;
         search: CheepListState;
+        quotes: CheepListState;
     };
     userLists: {
         following: UserListState;
