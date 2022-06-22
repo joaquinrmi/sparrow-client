d141f58967b92c6078e414fcaed37aa02b61cdab
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 17:30:14 -0300

    Add Home page

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import CheepList from "../../components/cheep_list";
+import PageHeader from "../../components/page_header";
+
+import "./home.scss";
+
+export interface Props
+{}
+
+const Home: React.FunctionComponent<Props> = (props) =>
+{
+    return <div className="home-page">
+        <PageHeader>
+            <span className="title">Inicio</span>
+        </PageHeader>
+
+        <CheepList name="home" arguments={{
+            timeline: true
+        }} />
+    </div>;
+};
+
+export default Home;
