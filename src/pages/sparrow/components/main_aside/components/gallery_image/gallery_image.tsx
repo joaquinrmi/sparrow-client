49f9f59911cca7a2b4ce0a12a8326f75aa8b5347
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-22 16:42:11 -0300

    Add GalleryImage component

---
+++ src/pages/sparrow/components/main_aside/components/gallery_image/gallery_image.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export interface Props
+{
+    data: PhotoData;
+}
+
+const GalleryImage: React.FunctionComponent<Props> = (props) =>
+{
+    return <Link to={`/${props.data.authorHandle}/status/${props.data.cheepId}/photo/${props.data.index}`} className="gallery-image" style={{
+        backgroundImage: `url(${props.data.url})`
+    }}>
+        <img src={props.data.url} />
+    </Link>;
+};
+
+export interface PhotoData
+{
+    url: string;
+    authorHandle: string;
+    cheepId: number;
+    index: number;
+}
+
+export default GalleryImage;
---
+++ src/pages/sparrow/components/main_aside/components/gallery_image/index.tsx
@@ -0,0 +1,4 @@
+import GalleryImage from "./gallery_image";
+
+export default GalleryImage;
+export * from "./gallery_image";
