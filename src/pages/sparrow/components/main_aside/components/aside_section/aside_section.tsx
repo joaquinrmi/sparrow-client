0765d74be238333b16a968913b0ad52e7e071362
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-24 09:58:19 -0300

    Add 'className' to AsideSection Props interface

--- src/pages/sparrow/components/main_aside/components/aside_section/aside_section.tsx
+++ src/pages/sparrow/components/main_aside/components/aside_section/aside_section.tsx
@@ -6,11 +6,12 @@ export interface Props
 {
     children?: React.ReactNode;
     title: string;
+    className?: string;
 }
 
 const AsideSection: React.FunctionComponent<Props> = (props) =>
 {
-    return <div className="aside-section">
+    return <div className={`aside-section ${props.className ? props.className : ""}`}>
         <header className="section-title">
             {props.title}
         </header>
