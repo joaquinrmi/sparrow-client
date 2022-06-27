5520738bb383e60a3b54f71066b25daae67cc819
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-27 05:00:07 -0300

    Add sticky style to NavigationBar component

--- src/pages/sparrow/components/navigation_bar/navigation_bar.tsx
+++ src/pages/sparrow/components/navigation_bar/navigation_bar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 import "./navigation_bar.scss";
@@ -10,21 +10,108 @@ export interface Props
 
 const NavigationBar: React.FunctionComponent<Props> = (props) =>
 {
-    return <div className="navigation-bar">
-        <NavigationItem to="/home" icon="house" />
+    useEffect(() =>
+    {
+        const element = document.querySelector(".navigation-bar") as HTMLDivElement;
+        const separator = document.querySelector(".navigation-separator") as HTMLDivElement;
+        if(element === null || separator === null)
+        {
+            return;
+        }
 
-        <NavigationItem to="/explore" icon="hashtag" />
+        let lastScroll = window.scrollY;
+        let start = true;
+        let finish = false;
+        let up = false;
+        let down = false;
 
-        <NavigationItem to="/notifications" icon="bell" />
+        const onScroll = () =>
+        {
+            const box = element.getBoundingClientRect();
 
-        <NavigationItem to="/messages" icon="envelope" />
+            if(box.height < window.innerHeight)
+            {
+                return;
+            }
 
-        <NavigationItem to={`/${props.handle}`} subroutes icon="user" />
+            if(lastScroll - window.scrollY < 0 && !down && !finish)
+            {
+                separator.style.height = `${window.scrollY}px`;
+                element.style.top = "";
+                element.style.position = "relative";
 
-        <NavigationItem to="/settings" icon="gear" />
+                start = false;
+                up = false;
+                finish = false;
+                down = true;
+            }
 
-        <NavigationItem to="/compose/cheep" icon="feather" containerClassName="feather" linkClassName="big-blue" />
-    </div>;
+            if((-box.top) + window.innerHeight >= box.height)
+            {
+                element.style.top = `${box.top}px`;
+                element.style.position = "sticky";
+
+                start = false;
+                up = false;
+                down = false;
+                finish = true;
+            }
+
+            if(lastScroll - window.scrollY > 0 && !up && finish)
+            {
+                separator.style.height = `${window.scrollY - (box.height - window.innerHeight)}px`;
+                element.style.top = "";
+                element.style.position = "relative";
+
+                start = false;
+                down = false;
+                finish = false;
+                up = true;
+            }
+
+            if(box.top > 0 && !start)
+            {
+                separator.style.height = "0px";
+                element.style.position = "sticky";
+                element.style.top = `0px`;
+
+                down = false;
+                up = false;
+                finish = false;
+                start = true;
+            }
+
+            lastScroll = window.scrollY;
+        };
+
+        window.addEventListener("scroll", onScroll);
+
+        return () =>
+        {
+            window.removeEventListener("scroll", onScroll);
+        };
+    },
+    []);
+
+    return <>
+        <div className="navigation-separator"></div>
+        
+        <div className="navigation-bar">
+            <NavigationItem to="/home" icon="house" />
+
+            <NavigationItem to="/explore" icon="hashtag" />
+
+            <NavigationItem to="/notifications" icon="bell" />
+
+            <NavigationItem to="/messages" icon="envelope" />
+
+            <NavigationItem to={`/${props.handle}`} subroutes icon="user" />
+
+            <NavigationItem to="/settings" icon="gear" />
+
+            <NavigationItem to="/compose/cheep" icon="feather" containerClassName="feather" linkClassName="big-blue" />
+        </div>
+    </>;
 };
 
 interface ItemProps
