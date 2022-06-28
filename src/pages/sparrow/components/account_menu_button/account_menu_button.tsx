d5135b3140f8fbcdf27b06bfdd9ea36e44c0b643
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-27 21:20:41 -0300

    Open AccountMenu on click

--- src/pages/sparrow/components/account_menu_button/account_menu_button.tsx
+++ src/pages/sparrow/components/account_menu_button/account_menu_button.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import SessionContext from "../../../../session_context";
+import StateContext from "../../state_context";
 
 import "./account_menu_button.scss";
 
@@ -9,8 +10,25 @@ export interface Props
 const AccountMenuButton: React.FunctionComponent<Props> = (props) =>
 {
     const userSession = useContext(SessionContext);
+    const [ state, stateManager ] = useContext(StateContext);
 
-    return <div className="account-menu-button">
+    return <div className="account-menu-button" onClick={
+        () =>
+        {
+            const button = document.querySelector(".account-menu-button") as HTMLDivElement;
+
+            if(button === null)
+            {
+                return;
+            }
+
+            const rect = button.getBoundingClientRect();
+            const x = rect.x + window.scrollX;
+            const y = rect.y + window.scrollY;
+
+            stateManager.openAccountMenu([ x, y ]);
+        }
+    }>
         <div className="button-image">
             <img src={userSession.user.picture} alt={`Foto de perfil de @${userSession.user.handle}`} />
         </div>
