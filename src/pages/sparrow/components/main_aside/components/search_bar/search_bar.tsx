ba6759e9a3c38991d5f4086ed7702fea09831e6c
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-30 14:16:55 -0300

    Fix white spaces

--- src/pages/sparrow/components/main_aside/components/search_bar/search_bar.tsx
+++ src/pages/sparrow/components/main_aside/components/search_bar/search_bar.tsx
@@ -13,72 +13,78 @@ const SearchBar: React.FunctionComponent<Props> = (props) =>
 {
     const navigate = useNavigate();
 
-    useEffect(() =>
-    {
-        const element = document.getElementById(props.id) as HTMLDivElement;
-        if(element === null)
+    useEffect(
+        () =>
         {
-            return;
-        }
+            const element = document.getElementById(props.id) as HTMLDivElement;
+            if(element === null)
+            {
+                return;
+            }
 
-        const inputElement = element.querySelector("input") as HTMLInputElement;
-        if(inputElement === null)
+            const inputElement = element.querySelector("input") as HTMLInputElement;
+            if(inputElement === null)
+            {
+                return;
+            }
+
+            const onClick = () =>
+            {
+                inputElement.focus();
+            };
+
+            const onFocus = () =>
+            {
+                element.classList.add("active");
+            };
+
+            const onFocusOut = () =>
+            {
+                element.classList.remove("active");
+            };
+
+            element.addEventListener("click", onClick);
+            inputElement.addEventListener("focusin", onFocus);
+            inputElement.addEventListener("focusout", onFocusOut);
+
+            return () =>
+            {
+                element.removeEventListener("click", onClick);
+                inputElement.removeEventListener("focusin", onFocus);
+                inputElement.removeEventListener("focusout", onFocusOut);
+            };
+        },
+        [ props.id ]
+    );
+
+    return <div
+        id={props.id}
+        className="search-bar-container"
+        onKeyDown={(ev) =>
         {
-            return;
-        }
+            const element = document.getElementById(props.id) as HTMLDivElement;
+            if(element === null)
+            {
+                return;
+            }
 
-        const onClick = () =>
-        {
-            inputElement.focus();
-        };
+            const inputElement = element.querySelector("input") as HTMLInputElement;
+            if(inputElement === null)
+            {
+                return;
+            }
 
-        const onFocus = () =>
-        {
-            element.classList.add("active");
-        };
+            if(inputElement.value.trim().length === 0)
+            {
+                return;
+            }
 
-        const onFocusOut = () =>
-        {
-            element.classList.remove("active");
-        };
-
-        element.addEventListener("click", onClick);
-        inputElement.addEventListener("focusin", onFocus);
-        inputElement.addEventListener("focusout", onFocusOut);
-
-        return () =>
-        {
-            element.removeEventListener("click", onClick);
-            inputElement.removeEventListener("focusin", onFocus);
-            inputElement.removeEventListener("focusout", onFocusOut);
-        };
-    },
-    [ props.id ]);
-
-    return <div id={props.id} className="search-bar-container" onKeyDown={(ev) =>
-    {
-        const element = document.getElementById(props.id) as HTMLDivElement;
-        if(element === null)
-        {
-            return;
-        }
-
-        const inputElement = element.querySelector("input") as HTMLInputElement;
-        if(inputElement === null)
-        {
-            return;
-        }
-
-        if(inputElement.value.trim().length === 0)
-        {
-            return;
-        }
-
-        if(ev.key === "Enter")
-        {
-            navigate(`/search?q=${inputElement.value}`);
-        }
-    }}>
+            if(ev.key === "Enter")
+            {
+                navigate(`/search?q=${inputElement.value}`);
+            }
+        }}
+    >
         <div className="icon">
             <i className="fa-solid fa-magnifying-glass"></i>
         </div>
