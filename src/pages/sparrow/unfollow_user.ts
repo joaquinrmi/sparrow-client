a76f0ecf6856803f96e97e99f45030890de43e8f
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-30 13:53:54 -0300

    Fix white spaces

--- src/pages/sparrow/unfollow_user.ts
+++ src/pages/sparrow/unfollow_user.ts
@@ -2,16 +2,22 @@ async function unfollowUser(userHandle: string): Promise<boolean>
 {
     const unfollowURL = `${process.env.REACT_APP_SERVER}/api/user/unfollow`;
 
-    const response = await fetch(unfollowURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            userHandle
-        }),
-        credentials: "include"
-    });
+    const response = await fetch(
+        unfollowURL,
+        {
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    userHandle
+                }
+            ),
+            credentials: "include"
+        }
+    );
 
     return response.status === 200;
 }
