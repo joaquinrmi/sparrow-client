f60ef6da7b47391801f5cc8e20e02c6a20d5f0be
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-30 13:13:00 -0300

    Fix white space

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -24,19 +24,24 @@ const Gallery: React.FunctionComponent<Props> = (props) =>
 
     const pictures: Array<React.ReactNode> = [ null, null, null, null ];
     const pattern = GALLERY_PATTERN[props.pictures.length - 1];
+
     for(let i = 0; i < props.pictures.length; ++i)
     {
         const index = pattern[i];
-        pictures[index] = <SinglePicture key={`${i}-picture`} picture={props.pictures[i]} onClick={(ev) =>
-        {
-            if(!props.disableClick && props.cheepId !== undefined && props.userHandle !== undefined)
+        pictures[index] = <SinglePicture
+            key={`${i}-picture`}
+            picture={props.pictures[i]}
+            onClick={(ev) =>
             {
-                ev.preventDefault();
-                ev.stopPropagation();
+                if(!props.disableClick && props.cheepId !== undefined && props.userHandle !== undefined)
+                {
+                    ev.preventDefault();
+                    ev.stopPropagation();
 
-                navigate(`/${props.userHandle}/status/${props.cheepId}/photo/${index + 1}`);
-            }
-        }} />;
+                    navigate(`/${props.userHandle}/status/${props.cheepId}/photo/${index + 1}`);
+                }
+            }}
+        />;
     }
 
     return <div className={`gallery ${props.disableClick ? "disabled" : ""}`}>
