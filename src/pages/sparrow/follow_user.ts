04e62a043565fe68b9ae9018226a733162136ff5
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-30 13:44:56 -0300

    Fix white spaces

--- src/pages/sparrow/follow_user.ts
+++ src/pages/sparrow/follow_user.ts
@@ -2,16 +2,22 @@ async function followUser(userHandle: string): Promise<boolean>
 {
     const followURL = `${process.env.REACT_APP_SERVER}/api/user/follow`;
 
-    const response = await fetch(followURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            userHandle
-        }),
-        credentials: "include"
-    });
+    const response = await fetch(
+        followURL,
+        {
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(
+                {
+                    userHandle
+                }
+            ),
+            credentials: "include"
+        }
+    );
 
     return response.status === 201;
 }
