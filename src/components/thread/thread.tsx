e37b3e39d25889ef52d15dced4fdf67915e633c8
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-30 13:18:47 -0300

    Fix white spaces

--- src/components/thread/thread.tsx
+++ src/components/thread/thread.tsx
@@ -15,10 +15,12 @@ const Thread: React.FunctionComponent<Props> = (props) =>
     const cheepList = state.cheepLists[props.name];
 
     return <>
-        {cheepList.cheeps.map((cheepData, index) =>
-        {
-            return <Cheep key={`${index}-cheep`} id={`${index}-cheep-${props.name}`} data={cheepData} index={index} listName={props.name} />;
-        })}
+        {cheepList.cheeps.map(
+            (cheepData, index) =>
+            {
+                return <Cheep key={`${index}-cheep`} id={`${index}-cheep-${props.name}`} data={cheepData} index={index} listName={props.name} />;
+            }
+        )}
     </>;
 }
 
