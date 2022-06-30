e155c449e6e7000ec655afef552b4e97a3524f68
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-30 16:26:38 -0300

    Fix white spaces

--- src/pages/sparrow/components/recheep_button/recheep_button.tsx
+++ src/pages/sparrow/components/recheep_button/recheep_button.tsx
@@ -17,24 +17,37 @@ const RecheepButton: React.FunctionComponent<Props> = (props) =>
 {
     const [ state, stateManager ] = useContext(StateContext);
     
-    return <InteractionButton id={props.id} className="recheep-button" color={InteractionColor.Green} icon="retweet" counter={props.counter} active={props.active} onClick={(event) =>
-    {
-        event.stopPropagation();
-
-        const button = document.getElementById(props.id) as HTMLDivElement;
-
-        const rect = button.getBoundingClientRect();
-        const x = rect.x + window.scrollX;
-        const y = rect.y + window.scrollY;
-
-        stateManager.openRecheepMenu(props.cheepData, props.active || false, [ x, y ], () =>
+    return <InteractionButton
+        id={props.id}
+        className="recheep-button"
+        color={InteractionColor.Green}
+        icon="retweet"
+        counter={props.counter}
+        active={props.active}
+        onClick={(event) =>
         {
-            if(props.onRecheep)
-            {
-                props.onRecheep();
-            }
-        });
-    }} />;
+            event.stopPropagation();
+
+            const button = document.getElementById(props.id) as HTMLDivElement;
+
+            const rect = button.getBoundingClientRect();
+            const x = rect.x + window.scrollX;
+            const y = rect.y + window.scrollY;
+
+            stateManager.openRecheepMenu(
+                props.cheepData,
+                props.active || false,
+                [ x, y ],
+                () =>
+                {
+                    if(props.onRecheep)
+                    {
+                        props.onRecheep();
+                    }
+                }
+            );
+        }}
+    />;
 };
 
 export default RecheepButton;
